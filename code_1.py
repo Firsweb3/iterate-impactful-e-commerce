4eb19eb086bcbdf48f7e6de42a11323546871da3
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-09-22 03:17:08 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,69 +1,29 @@
-
-}
-}
-    for (let i = 0; i < 10; i++) {
-    return data;
-function generateRandomData() {
-    const data = generateRandomData();
-    const data = [];
-    data.forEach(item => {
+def main():
+def generate_random_data():
+    data = generate_random_data()
+if __name__ == "__main__":
+    main()
+import random
+        print(f"Random Number: {item}")
 
 
-        console.log("Random Number: " + item);
-    }
-function main() {
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data        console.log("Random Number: " + item);
         data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    return data;
+main();
+    for (let i = 0; i < 10; i++) {
+
+}
+    const data = generateRandomData();
+function main() {
+}
     });
-main();    for item in data:
-    main()
-def main():
-
-
-    data = generate_random_data()
-
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-def generate_random_data():
-    return data
-import random    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def generate_random_data():
-    for item in data:
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-import random
-
-def main():
-
-
-    main()
-    return data
-import random
-
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-if __name__ == "__main__":
-
-
-def main():
-
-    data = generate_random_data()
-    for item in data:def main():
-
-    for item in data:
-    main()
-    data = generate_random_data()
-    return data
-
-
-        print(f"Random Number: {item}")
-import random
-def generate_random_data():
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
+    const data = [];
+function generateRandomData() {
+    data.forEach(item => {
